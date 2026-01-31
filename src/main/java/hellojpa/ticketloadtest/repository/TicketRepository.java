528f9ab8f944ccece7c2c431db0cd22deaab01ff
LeeChangHyeong <chxngbro@gmail.com>
2026-01-31 15:20:48 +0900

    [revert] DB 비관적 락 제거 (Redis 분산 락 적용을 위한 원복)

--- src/main/java/hellojpa/ticketloadtest/repository/TicketRepository.java
+++ src/main/java/hellojpa/ticketloadtest/repository/TicketRepository.java
@@ -3,19 +3,7 @@ package hellojpa.ticketloadtest.repository;
 import hellojpa.ticketloadtest.domain.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import jakarta.persistence.LockModeType;
-
-import org.springframework.data.jpa.repository.Lock;
-
-import org.springframework.data.jpa.repository.Query;
-
-import org.springframework.data.repository.query.Param;
-
-
-
 import java.util.List;
-
-import java.util.Optional;
 
 
 
@@ -23,12 +11,6 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     List<Ticket> findAllByOrderByIdAsc();
 
-
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-
-    @Query("select t from Ticket t where t.id = :id")
-
-    Optional<Ticket> findByIdWithLock(@Param("id") Long id);
-
 }
+
+
